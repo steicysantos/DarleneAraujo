aee57e9ad5ddafa50ab79b8fb56e8a6910c1bf13
steicy <steicymsantos@gmail.com>
2023-01-18 13:26:52 -0300

    ajsdoj

---
+++ curso-presencial/script.js
@@ -0,0 +1,24 @@
+let count = 1
+
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+  nextImage();
+}, 7000)
+
+function nextImage(){
+  count++;
+  if(count>4){
+    count=1;
+  }
+
+  document.getElementById("radio"+count).checked = true;
+}
+
+const accordion = document.getElementsByClassName('contentBx');
+
+for(i = 0; i < accordion.length; i++){
+  accordion[i].addEventListener('click', function(){
+    this.classList.toggle('active')
+  })
+}
