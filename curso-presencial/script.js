d459c2c0184e3f2ffab61c09424d617f394608b3
steicy <steicymsantos@gmail.com>
2023-01-24 14:27:56 -0300

    sunaau

--- curso-presencial/script.js
+++ curso-presencial/script.js
@@ -22,3 +22,25 @@ for(i = 0; i < accordion.length; i++){
     this.classList.toggle('active')
   })
 }
+
+
+        
+        
+$(document).ready(function() {
+
+  $('.counter').each(function () {
+$(this).prop('Counter',0).animate({
+  Counter: $(this).text()
+}, {
+  duration: 4000,
+  easing: 'swing',
+  step: function (now) {
+      $(this).text(Math.ceil(now));
+  }
+});
+});
+
+});  
+
+
+
