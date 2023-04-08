176bbc1349bf8fd6f8fe0ee6721f40a21d3bd09e
Nathan <Nathan.ansayc@gmail.com>
2023-04-08 01:25:02 -0300

    Carrossel insta inicio - video

--- script.js
+++ script.js
@@ -21,6 +21,31 @@
     }
   });
 
+  $('.owl-insta').owlCarousel({
+    loop: true,
+    margin: 5,
+    nav: true,
+    navText: [
+      "<i class='fa fa-caret-left'></i>",
+      "<i class='fa fa-caret-right'></i>"
+    ],
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      },  
+      1500: {
+        items: 4
+      }
+    }
+  });
+
   $('.owl-two').owlCarousel({
     loop: true,
     margin: 5,
