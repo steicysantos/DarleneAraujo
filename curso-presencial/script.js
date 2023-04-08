1c32753d2a2795c2622dec311ebeadd3758badc9
steicy <steicymsantos@gmail.com>
2023-04-07 23:40:17 -0300

    meu deus q inferno

--- curso-presencial/script.js
+++ curso-presencial/script.js
@@ -26,21 +26,3 @@ for(i = 0; i < accordion.length; i++){
 
         
         
-$(document).ready(function() {
-
-  $('.counter').each(function () {
-$(this).prop('Counter',0).animate({
-  Counter: $(this).text()
-}, {
-  duration: 4000,
-  easing: 'swing',
-  step: function (now) {
-      $(this).text(Math.ceil(now));
-  }
-});
-});
-
-});  
-
-
-
