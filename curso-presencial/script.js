c1f58e00bfe82b1e266e4002fbe50442e9f0d996
steicy <steicymsantos@gmail.com>
2023-04-14 09:22:09 -0300

    hsaud

--- curso-presencial/script.js
+++ curso-presencial/script.js
@@ -1,27 +1,78 @@
-// let count = 1
+var slideIndex = 0;
+var slides = document.getElementsByClassName("slide");
+var dots = document.getElementsByClassName("dot");
 
-// document.getElementById("radio1").checked = true;
+showSlides();
 
-// setInterval(function(){
-//   nextImage();
-// }, 7000)
+function showSlides() {
+  var i;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 5000); // Muda o slide a cada 5 segundos
+}
 
-// function nextImage(){
-//   count++;
-//   if(count>4){
-//     count=1;
-//   }
+// Adiciona evento de clique aos dots
+for (var i = 0; i < dots.length; i++) {
+  dots[i].addEventListener("click", function () {
+    var dotIndex = Array.prototype.indexOf.call(dots, this);
+    slideIndex = dotIndex + 1;
+    showSlides();
+  });
+}
 
-//   document.getElementById("radio"+count).checked = true;
-// }
+const accordionItems = document.querySelectorAll('.accordion-item');
 
-// const accordion = document.getElementsByClassName('contentBx');
+accordionItems.forEach(item => {
+  const header = item.querySelector('.accordion-header');
+  header.addEventListener('click', () => {
+    const isActive = item.classList.contains('active');
+    accordionItems.forEach(item => item.classList.remove('active'));
+    if (!isActive) {
+      item.classList.add('active');
+    }
+  });
+});
 
-// for(i = 0; i < accordion.length; i++){
-//   accordion[i].addEventListener('click', function(){
-//     this.classList.toggle('active')
-//   })
-// }
+window.addEventListener('load', () => {
+  let counter1 = 0;
+  let counter2 = 0;
+  let counter3 = 0;
+  let counter4 = 0;
+
+  const target1 = 78;
+  const target2 = 1000;
+  const target3 = 10;
+  const target4 = 70;
+
+  setInterval(() => {
+    if (counter1 < target1) {
+      counter1++;
+      document.getElementById('counter1').innerText = counter1;
+    }
+    if (counter2 < target2) {
+      counter2++;
+      document.getElementById('counter2').innerText = counter2;
+    }
+    if (counter3 < target3) {
+      counter3++;
+      document.getElementById('counter3').innerText = counter3;
+    }
+    if (counter4 < target4) {
+      counter4++;
+      document.getElementById('counter4').innerText = counter4;
+    }
+  }, 10);
+});
 
 console.log("Nathan")
 
