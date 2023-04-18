e7fcdff9f0cebc4a953b613c9a42ac53aee40839
Nathan <Nathan.ansayc@gmail.com>
2023-04-18 10:46:38 -0300

    Arrumando Presencial

--- Presencial/script.js
+++ Presencial/script.js
@@ -16,23 +16,23 @@ function prev(){
 setInterval(next, 7000);
 
 $('.owl-one').owlCarousel({
-    loop: true,
-    margin: 5,
-    nav: true,
-    navText: [
-      "<i class='fa fa-caret-left'></i>",
-      "<i class='fa fa-caret-right'></i>"
-    ],
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 2
-      },
-      1000: {
-        items: 3
-      }
+  loop: true,
+  margin: 5,
+  nav: true,
+  navText: [
+    "<i class='fa fa-caret-left'></i>",
+    "<i class='fa fa-caret-right'></i>"
+  ],
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 2
+    },
+    1500: {
+      items: 3
     }
-  });
+  }
+});
