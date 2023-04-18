a451fe5e0231bfc893e8d791e6dce6d17e86af90
steicy <steicymsantos@gmail.com>
2023-04-18 10:09:42 -0300

    hushsau

---
+++ Presencial/script.js
@@ -0,0 +1,38 @@
+let slides = document.querySelectorAll('.slider');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+setInterval(next, 7000);
+
+$('.owl-one').owlCarousel({
+    loop: true,
+    margin: 5,
+    nav: true,
+    navText: [
+      "<i class='fa fa-caret-left'></i>",
+      "<i class='fa fa-caret-right'></i>"
+    ],
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
--- curso-presencial/script.js
+++ curso-presencial/script.js
@@ -49,7 +49,22 @@ $('.owl-one').owlCarousel({
   }
 });
 
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
 
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+setInterval(next, 7000);
 
 // var slideIndex = 0;
 // var slides = document.getElementsByClassName("slide");
